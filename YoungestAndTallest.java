86e3024c6d3c4f46b7568bfc55b9009919f56560
Yagyata <yagyatasharmaa@gmail.com>
2025-03-03 22:49:31 +0530

    [Yagyata] ADD: Finding the youngest and tallest friend

---
+++ YoungestAndTallest.java
@@ -0,0 +1,39 @@
+import java.util.Scanner;
+
+public class YoungestAndTallest {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String[] friends = {"Amar", "Akbar", "Anthony"};
+        int[] ages = new int[3];
+        double[] heights = new double[3];
+
+        // Taking input for ages and heights
+        for (int i = 0; i < 3; i++) {
+            System.out.print("Enter age of " + friends[i] + ": ");
+            ages[i] = sc.nextInt();
+            System.out.print("Enter height (in cm) of " + friends[i] + ": ");
+            heights[i] = sc.nextDouble();
+        }
+
+        // Finding the youngest
+        int youngestIndex = 0;
+        for (int i = 1; i < 3; i++) {
+            if (ages[i] < ages[youngestIndex]) {
+                youngestIndex = i;
+            }
+        }
+
+        // Finding the tallest
+        int tallestIndex = 0;
+        for (int i = 1; i < 3; i++) {
+            if (heights[i] > heights[tallestIndex]) {
+                tallestIndex = i;
+            }
+        }
+
+        // Displaying the results
+        System.out.println("\nThe youngest friend is " + friends[youngestIndex]);
+        System.out.println("The tallest friend is " + friends[tallestIndex]);
+
+    }
+}
