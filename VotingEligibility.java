51fcdd4f2d3c19959c2424242746f0aaedf8ce6c
Yagyata <yagyatasharmaa@gmail.com>
2025-03-03 14:16:56 +0530

    [Yagyata] ADD: Checking the voting eligibility of 10 students

--- VotingEligibility.java
+++ VotingEligibility.java
@@ -12,15 +12,16 @@ public class VotingEligibility{
 
 		//Checking the eligibility of each student using for loop and using conditional operators
 		for(int i=0; i<age.length; i++){
-			if (age[i] >= 18) {
+			if (age[i] <= 0 || age[i] >=110) {
+				System.err.println("Invalid Age");
+			}
+			else if (age[i] >= 18) {
 				System.out.println("The Student with age " + age[i] + " can vote");
 			}
-			else if (age[i] < 18) {
-				System.out.println("The student with age " + age[i] + " cannot vote");
-			}
 			else {
-			System.err.println("Invalid Age");
+				System.out.println("The student with age " + age[i] + " cannot vote");
 			}
 		}
 	}
 }
+
